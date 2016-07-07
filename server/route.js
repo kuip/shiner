c798d6c249676fa107e608c046dff0da8d938072
Loredana Cirstea <loredana.cirstea@gmail.com>
2016-07-07 17:33:21 +0300

    prepare server routes

---
+++ server/route.js
@@ -0,0 +1,11 @@
+/*
+Picker.route('/source/:type/:app/:page?', function(params, req, res, next) {
+  var post = Posts.findOne(params._id);
+  res.end(post.content);
+});
+
+Picker.route('/preview/:app/:page?', function(params, req, res, next) {
+  var post = Posts.findOne(params._id);
+  res.end(post.content);
+});
+*/
